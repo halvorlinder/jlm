9c34626eec889d592469ad41e9210970c4316323
Halvor Linder <halvor_lh@hotmail.no>
2025-01-28 14:07:53 +0100

    delta: Add missing finalization

--- jlm/mlir/frontend/MlirToJlmConverter.cpp
+++ jlm/mlir/frontend/MlirToJlmConverter.cpp
@@ -678,7 +678,10 @@ MlirToJlmConverter::ConvertOperation(
         mlirDeltaNode.getSection().str(),
         mlirDeltaNode.getConstant());
 
-    ConvertRegion(mlirDeltaNode.getRegion(), *rvsdgDeltaNode->subregion());
+    auto outputVector = ConvertRegion(mlirDeltaNode.getRegion(), *rvsdgDeltaNode->subregion());
+    JLM_ASSERT(outputVector.size() == 1);
+
+    rvsdgDeltaNode->finalize(outputVector[0]);
 
     return rvsdgDeltaNode;
   }
